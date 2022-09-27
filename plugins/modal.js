87dd3aa5cb11082291dde5a0475be2b79f2bffb8
SoulSleepy <0149532@gmail.com>
2022-09-27 20:02:48 +0300

    open/close modal

--- plugins/modal.js
+++ plugins/modal.js
@@ -1,9 +1,38 @@
+function _createModal(options) {
+    const modal = document.createElement('div')
+    modal.classList.add('modal-my')
+    modal.insertAdjacentHTML('afterbegin', `
+        <div class="modal-overlay">
+            <div class="modal-window">
+                <div class="modal-header">
+                    <span class="modal-title">Modal title</span>
+                    <span class="modal-close">&times;</span>
+                </div>
+                <div class="modal-body">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div class="modal-footer">
+                    <button>Ok</button>
+                    <button>Close</button>
+                </div>
+            </div>
+        </div>
+    `)
+    document.body.appendChild(modal)
+    return modal
+}
+
+
 $.modal = function (options) {
-
-
-    return (
-        open() { },
-        close() { },
+    const $modal = _createModal(options)
+    return {
+        open() {
+            $modal.classList.add('open')
+        },
+        close() {
+            $modal.classList.remove('open')
+        },
         destroy() { }
-    )
+    }
 }
